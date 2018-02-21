dad5a6d436dea2b9166b00161967dd9cf3224423
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-21 16:19:34 +0300

    fix

--- back/app/routes.py
+++ back/app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 @app.route('/')
 def index():
-    return render_template('../front/index.html')
+    return render_template(basedir + '../front/index.html')

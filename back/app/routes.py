73d2dbade90e5ef17cf63273ec3b019e560c749b
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-21 16:22:32 +0300

    fix

--- back/app/routes.py
+++ back/app/routes.py
@@ -6,4 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 @app.route('/')
 def index():
-    return render_template(basedir + '../front/index.html')
+    return render_template(basedir + '/../front/index.html')

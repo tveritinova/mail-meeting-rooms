bdf507d7beca811763728dddd3aa7a8ec208d977
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-21 19:34:56 +0300

    back to html link css

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 import os
 
 template_dir = os.path.abspath('../front')
-static_dir = os.path.abspath('../front/dist')
+static_dir = os.path.abspath('../front')
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 app.config.from_object(Config)

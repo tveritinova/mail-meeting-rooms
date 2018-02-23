3c39f2e2dc57b8f2c407046b609c61d0f3cd8947
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-23 20:30:59 +0300

    fix

--- back/config.py
+++ back/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://'user+':'password+'@localhost'+os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql://'+user+':'password+'@localhost'+os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
     DEBUG = True

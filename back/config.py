e9ef9a1697ed5faa382d85c27093d8c88acc6237
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-23 20:30:26 +0300

    add user + password to mysql

--- back/config.py
+++ back/config.py
@@ -1,10 +1,11 @@
 import os
+from secure_info import user, password
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql://'user+':'password+'@localhost'+os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
     DEBUG = True

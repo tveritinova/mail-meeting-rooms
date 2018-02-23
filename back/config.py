4359ef65ecb0cc4e8327be81ddfe5a10d521a5b0
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-23 20:19:55 +0300

    fix db engine

--- back/config.py
+++ back/config.py
@@ -4,8 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql://' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
     DEBUG = True

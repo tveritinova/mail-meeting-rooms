b903c92e3194a7c9a2b43cb7c34e5e0e16b1689f
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-24 16:54:18 +0300

    fix

--- back/app/routes.py
+++ back/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, url_for
 from app import app, db
 from app.models import User, Room, Event
 import os
@@ -7,7 +7,8 @@ import json
 from datetime import datetime
 from flask_login import login_user
 from passlib.hash import pbkdf2_sha256
-# from token_ import generate_confirmation_token, confirm_token
+from token_ import generate_confirmation_token, confirm_token
+from flask.ext.mail import Message
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -43,32 +44,46 @@ def login():
 
 	return json.dumps({'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name}), 200
 
+def send_email(to, confirm_url):
+    msg = Message(
+        "Please confirm your email",
+        recipients=[to],
+        html="<a href="+confirm_url+">Подтвердить</a>",
+        sender=app.config['MAIL_DEFAULT_SENDER']
+    )
+    mail.send(msg)
+
 
 @app.route('/users', methods=['POST'])
 @cross_origin()
 def register():
 	data = json.loads(request.data)
-	print data
-	db.session.add(User(
+	user = User(
 		email=data['email'], 
 		password_hash=pbkdf2_sha256.encrypt(data['password']),
 		first_name=data['first_name'],
 		last_name=data['last_name'],
-		registered_on=datetime.now()))
+		registered_on=datetime.now())
+	db.session.add(user)
 	db.session.commit()
 
-	#token = generate_confirmation_token(user.email)
+	token = generate_confirmation_token(user.email)
+	confirm_url = url_for('user.confirm_email', token=token, _external=True)
+    send_email(user.email, confirm_url)
 
 	return '', 200
 
-'''
+
 @app.route('/confirm/<token>')
 def confirm(token):
     email = confirm_token(token)
     user = User.query.filter(User.email == email).one()
-    user.confirmed = True
-    user.confirmed_on = datetime.now()
-    db.session.commit()
-    return redirect(url_for('home'))
-'''
+
+    if not user.confirmed:
+	    user.confirmed = True
+	    user.confirmed_on = datetime.now()
+	    db.session.commit()
+
+    return 'confirmed', 200
+
 
--- back/config.py
+++ back/config.py
@@ -9,4 +9,17 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
     DEBUG = True
-    #SECURITY_PASSWORD_SALT = 'my_precious_two'
+    SECURITY_PASSWORD_SALT = 'my_precious_two'
+
+     # mail settings
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    # gmail authentication
+    MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
+
+    # mail accounts
+    MAIL_DEFAULT_SENDER = 'from@example.com'

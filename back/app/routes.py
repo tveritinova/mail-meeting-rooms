14eed39bc0b10c0bf668046b7bc7bf3120697dd7
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-24 19:29:34 +0300

    add get rooms

--- back/app/routes.py
+++ back/app/routes.py
@@ -21,12 +21,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 def index():
     return render_template('index.html')
 
-'''
+
 @app.route('/rooms', methods=['GET'])
 def get_rooms():
 	return Room.query.all()
 
-
+'''
 @app.route('/rooms', methods=['POST'])
 def post_rooms():
 	rooms = Room(name= , floor_num= )
